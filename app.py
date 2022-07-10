03fc1b6abcaaf98e8c3193f2a6f902b2d908b576
Haewon Shin <shinhae40@gmail.com>
2022-07-10 20:04:06 +0900

    add index favicon

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def decode() -> str:
     return render_template("decode.html")
 
+@app.route('/')
+def index() -> str:
+    return render_template("index.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
